e333fc3c6a2d9333b1479b09b56bf398f750e495
TzuruC <ostrich2821@gmail.com>
2023-11-18 13:29:25 +0800

    update all links

--- assets/js/tc-menu.js
+++ assets/js/tc-menu.js
@@ -1,11 +1,24 @@
 
 
-const navToggler = document.querySelector('.nav-toggler');
+const navToggler = document.querySelector('.nav-toggler span');
 const navGroup = document.querySelector('#navGroup');
-
+const transNav = document.querySelector('#transNav');
+let lastKnownScrollPosition = 0;
 
 navToggler.addEventListener('click', () => {
     navGroup.classList.toggle('d-none');
     navGroup.classList.toggle('fixed-top');
-    navGroup.setAttribute('style', 'top:7vh')
+    navGroup.classList.add('bg-dark');    
+    navGroup.setAttribute('style', 'top:7vh'); 
+    navGroup.setAttribute('transition', 'transform 0.3s ease-in-out, opacity 0.3s ease-in-out'); 
+    navToggler.textContent = (navToggler.textContent === 'close') ? 'menu' : 'close';    
+});
+
+document.addEventListener('scroll', (e) => {
+    lastKnownScrollPosition = window.scrollY;
+    if(lastKnownScrollPosition > 0){
+        transNav.classList.add('bg-dark-7');
+    }else if(lastKnownScrollPosition == 0){
+        transNav.classList.remove('bg-dark-7');
+    }
 });
