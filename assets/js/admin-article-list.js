1379a196ae3a7e06fe3ec0b084d48ca19d97dd49
Rxzan <ostrich2821@example.com>
2024-03-06 12:55:22 +0800

    刪除文章完成，套用sweetalert完成

--- assets/js/admin-article-list.js
+++ assets/js/admin-article-list.js
@@ -1,43 +1,56 @@
 import { api_url } from "./config.js";
+import Swal from 'sweetalert2';
+let deleteBtns;
 
 // 畫面初始化
-function init(){
-    getArticles();
-  };
-  init();
+function init() {
+  getArticles();
+};
+init();
 
 // 取得文章列表
 const articleListItem = document.querySelector('.js-articleList');
 let articleData = [];
-function getArticles(){
+function getArticles() {
   axios.get(`${api_url}/articles`)
-  .then(function (res) {
-    articleData = res.data;
-    let str = ``;
-    articleData.forEach((i) => {
+    .then(function (res) {
+      articleData = res.data;
+      let str = ``;
+      articleData.forEach((i) => {
         str += renderArticleList(i);
-    });
-    articleListItem.innerHTML = str;
-    getArticleCategories(res);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
+      });
+      articleListItem.innerHTML = str;
+      getArticleCategories(res);
+    })
+    .then(function (res) {
+      deleteBtns = document.querySelectorAll(".delete-btn");
+      deleteBtns.forEach(deleteBtn => {
+        deleteBtn.addEventListener("click", (e) => {
+          e.preventDefault();
+          const dataId = deleteBtn.getAttribute("data-id");
+          deleteArticle(dataId);
+        });
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
 }
 
-function renderArticleList(i){
+
+// 渲染文章列表
+function renderArticleList(i) {
   return `<tr>
         <th scope="row">${i.id}</th>
         <td>${i.articleName}</td>
         <td>${i.articleCategory}</td>
         <td>${i.articleAuther}</td>
         <td class="text-center">${i.articleCreatTime}</td>
-        <td class="text-center">${
-            i.articlePublic ? "公開" : "未公開"
-        }</td>
+        <td class="text-center">${i.articlePublic ? "公開" : "未公開"
+    }</td>
         <td class="text-center">
         <a class="link-primary" data-id="${i.id}" href="admin-article-detail.html?id=${i.id}">查看</a> / 
-        <a class="link-primary" data-id="${i.id}" href="">刪除</a></td>
+        <a class="delete-btn link-primary" data-id="${i.id}" href="">刪除</a></td>
     </tr>
     `;
 }
@@ -45,38 +58,58 @@ function renderArticleList(i){
 // 篩選文章-1
 // 渲染分類項目
 const articleFilter = document.querySelector('.admin-article-filter .form-select');
-function getArticleCategories(res){
-    // 取得所有分類
-    let articleAllCategoryData = [];
-    articleData = res.data;
-    articleData.forEach((i) => {   
-        articleAllCategoryData.push(i.articleCategory);     
-    });
-    // 使用 Set 去除重複值
-    let articleCategoryData = [...new Set(articleAllCategoryData)];
-    // 渲染分類項目
-    let str = `<option value="所有分類">- 所有分類 -</option>`;
-    articleCategoryData.forEach((i)=>{
-        str += `
+function getArticleCategories(res) {
+  // 取得所有分類
+  let articleAllCategoryData = [];
+  articleData = res.data;
+  articleData.forEach((i) => {
+    articleAllCategoryData.push(i.articleCategory);
+  });
+  // 使用 Set 去除重複值
+  let articleCategoryData = [...new Set(articleAllCategoryData)];
+  // 渲染分類項目
+  let str = `<option value="所有分類">- 所有分類 -</option>`;
+  articleCategoryData.forEach((i) => {
+    str += `
         <option value="${i}">${i}</option>        
         `;
-    });
-    articleFilter.innerHTML = str;
+  });
+  articleFilter.innerHTML = str;
 }
 // 篩選文章-2
-articleFilter.addEventListener('change',(e)=>{
+articleFilter.addEventListener('change', (e) => {
   e.preventDefault();
   const category = e.target.value;
-  if(category == "所有分類"){    
+  if (category == "所有分類") {
     getArticles();
     return;
   }
   let str = ``;
-  articleData.forEach((i)=>{
-    if(i.articleCategory == category){
-        str += renderArticleList(i);
+  articleData.forEach((i) => {
+    if (i.articleCategory == category) {
+      str += renderArticleList(i);
     }
   });
   articleListItem.innerHTML = str;
 });
 
+// 刪除文章
+function deleteArticle(dataId) {
+  axios.delete(`${api_url}/articles/${dataId}`)
+    .then(function (res) {
+      Swal.fire({
+        title: '刪除成功',
+        text: '即將返回文章列表',
+        icon: 'success',
+        confirmButtonText: '確認'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          location.reload();
+        }
+      })
+
+    })
+    .catch(error => {
+      console.error('刪除失敗︰', error);
+    });
+}
---
+++ assets/js/admin-article-new.js
@@ -0,0 +1,47 @@
+import { api_url } from "./config.js";
+import Swal from 'sweetalert2';
+
+
+
+const title = document.querySelector("#articleTitle");
+const category = document.querySelector("#articleType");
+const author = document.querySelector("#articleAuthor");
+const time = document.querySelector("#articleTime");
+const brief = document.querySelector("#articleBrief");
+const content = document.querySelector("#articleContent");
+const newBtn = document.querySelector("#newArticle");
+
+
+
+// 取得input內容
+let articleData = {};
+// post到json
+
+newBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    articleData = {
+        "articleName": title.value,
+        "articleCategory": category.value,
+        "articleAuther": author.value,
+        "articleCreatTime": time.value,
+        "articleBrief": brief.value,
+        "articleContent": content.value,
+    };
+    axios.post(`${api_url}/articles`, articleData)
+        .then(function (res) {
+            Swal.fire({
+                title: '新增成功',
+                text: '即將返回文章列表',
+                icon: 'success',
+                confirmButtonText: '確認'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = 'admin-article-list.html';
+                }
+            })
+        })
+        .catch(function (error) {
+            console.error("發生錯誤:", error);
+        });
+})
+
