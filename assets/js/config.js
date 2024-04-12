984b124879c94771456c1c7d7d220c8574542117
Rumi <ostrich2821@gmail.com>
2024-04-12 13:55:39 +0800

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,2 +1,2 @@
-export const api_url = "http://localhost:3000";
-// export const api_url = "https://fitnee-36.onrender.com";
+// export const api_url = "http://localhost:3000";
+export const api_url = "https://fitnee-36.onrender.com";
